475da0800be6dd8fba74ea3956588da5819c30f2
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-31 22:10:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const fname = document.getElementById("fname");
+	const lname = document.getElementById("lname");
+	const submit = document.getElementById("submit");
+	fname.value = "Rishabh";
+	lname.value = "Pant";
+	alert(fname.value + lname.value);
 
 }
